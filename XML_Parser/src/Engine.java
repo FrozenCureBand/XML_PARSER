e86b39065438853db2b4229406fd2b0b4f1d43de
FrozenCureBand <31348532+FrozenCureBand@users.noreply.github.com>
2020-03-28 23:35:20 +0100

    Edit del metodo CorrectTag

--- XML_Parser/src/Engine.java
+++ XML_Parser/src/Engine.java
@@ -3,6 +3,7 @@ import javax.swing.JOptionPane;
 
 public class Engine {
 
+//Metodo que detectar si ambos tags son iguales para detectarlos como un elemento padre o hijo.	
 public static boolean CorrectTag(String o, String c) {
 	
 		if (o.equals(c)) {
